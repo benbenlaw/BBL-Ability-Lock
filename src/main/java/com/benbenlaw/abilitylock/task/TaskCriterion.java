78cceaa1afdfbfa4855bf8439cff4d47bb94bc55
benbenlaw <benlawrenceben@hotmail.co.uk>
2026-07-31 10:42:21 +0100

    1.2.0

--- src/main/java/com/benbenlaw/abilitylock/task/TaskCriterion.java
+++ src/main/java/com/benbenlaw/abilitylock/task/TaskCriterion.java
@@ -6,7 +6,6 @@ public interface TaskCriterion {
     TaskCriterionType type();
     int target();
     int amountFor(TaskEvent event);
-
     default int countInInventory(ServerPlayer player) {
         return -1;
     }
